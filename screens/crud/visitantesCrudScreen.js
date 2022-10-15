54ce6877f2f494ca7fa08781fc6cb1f7f63e135e
FelipeGUI15 <72278871+FelipeGUI15@users.noreply.github.com>
2022-10-15 19:27:57 -0300

    Update visitantesCrudScreen.js

--- screens/crud/visitantesCrudScreen.js
+++ screens/crud/visitantesCrudScreen.js
@@ -1,9 +1,32 @@
-import { Button , View } from 'react-native';
+import { Button , View, Text, TextInput } from 'react-native';
 
 export function VisitantesCrudScreen({ navigation: { goBack } }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => goBack()} title="Cancel" />
+
+      <TextInput placeholder='Nome'></TextInput>
+      
+    <View >
+    
+      <Button onPress={() => goBack()} title="Voltar" />
+      <Button onPress={() => goBack()} title="Salvar" />
+   
     </View>
+      <TextInput placeholder='CPF'></TextInput>
+      <TextInput placeholder='Telefone'></TextInput>
+      <TextInput placeholder='Sexo'></TextInput>
+      <TextInput placeholder='E-mail'></TextInput>
+      
+    
+     </View>
+
   );
+  <html lang="en">
+    <head>
+      <div>
+        <TextInput placeholder='CPF'></TextInput>
+      </div>
+    </head>
+
+  </html>
 }
